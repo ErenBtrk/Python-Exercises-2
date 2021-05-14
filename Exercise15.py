09a27a157238d92b35bc8a1eaf3285f3adb561c7
ErenBtrk <erenbtrk@hotmail.com>
2021-05-14 20:46:33 +0300

    Exercise 15

---
+++ Exercise15.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program to get the volume of a sphere with radius 6.
+
+'''
+
+r = 6
+pi = 3.14
+volume = 4/3*pi*r**3
+
+print(f"Volume of a sphere with radius {r} is {volume:.2f}")
