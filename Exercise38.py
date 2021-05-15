37bc57d8422480df4b90982459e10710743b1250
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 15:13:32 +0300

    Exercise 38

---
+++ Exercise38.py
@@ -0,0 +1,11 @@
+'''
+38. Write a Python program to solve (x + y) * (x + y). Go to the editor
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
+
+'''
+
+def function(x,y):
+    return (x+y)**2
+
+print(function(1,2)) 
---
+++ Exercise39.py
@@ -0,0 +1,5 @@
+'''
+39. Write a Python program to compute the future value of a specified principal amount,
+ rate of interest, and a number of years.
+ 
+ '''
