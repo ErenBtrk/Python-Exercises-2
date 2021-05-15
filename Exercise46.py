92cf0f7b23c10bedffa0fd8486765a333ed9bbf6
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 21:05:27 +0300

    Exercise 46

---
+++ Exercise46.py
@@ -0,0 +1,10 @@
+'''
+46. Write a python program to get the path and name of the file
+ that is currently executing.
+ 
+'''
+
+import os
+print("Current File Name : ",os.path.realpath(__file__))
+
+print("Current File Name : ",os.path.realpath("Exercise1.py"))
