10ebf5a4a512196f40b3a54271f272420b2bfac6
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 15:28:26 +0300

    Exercise 39

--- Exercise39.py
+++ Exercise39.py
@@ -3,3 +3,10 @@
  rate of interest, and a number of years.
  
  '''
+
+def function(value,rate,years):
+    return value*(1+rate*0.01)**years
+
+
+future = function(10000,3.5,7)
+print(f"{future:.2f}")
