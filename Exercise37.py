5e4b2befa78425006f08b435081643f7a4eb1363
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 15:10:10 +0300

    Exercise 37

---
+++ Exercise37.py
@@ -0,0 +1,10 @@
+'''
+37. Write a Python program to display your details like name, age, address in three different lines. 
+
+'''
+
+name = input("Please enter your name : ")
+age = int(input("Please enter your age : "))
+adress = input("Please enter your adress : ")
+
+print(f"{name}\n{age}\n{adress}")
